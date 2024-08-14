645496e6da4cfbd0da9fc4e3601f095740d66081
[Rasmus Krogh-Andersen] <[rasmus.kroghandersen@gmail.com]>
2024-08-14 14:33:14 +0200

    added js file for routing

---
+++ routing.js
@@ -0,0 +1,25 @@
+const logo = document.querySelector(".logo-section");
+const filter = document.querySelector(".filter-section");
+const movie = document.querySelector("#movie");
+const list = document.querySelector("#list");
+const user = document.querySelector("#user");
+
+const routing = () => {
+  logo.addEventListener("click", () => {
+    window.location.href = "./index.html";
+  });
+  filter.addEventListener("click", () => {
+    window.location.href = "./filter.html";
+  });
+  movie.addEventListener("click", () => {
+    window.location.href = "./index.html";
+  });
+  list.addEventListener("click", () => {
+    window.location.href = "./list.html";
+  });
+  user.addEventListener("click", () => {
+    window.location.href = "./account.html";
+  });
+};
+
+routing();
